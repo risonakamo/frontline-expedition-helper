d885f4bd6ad2719e213c0c3706da7049f8d38ede
risonakamo <hmykanae@gmail.com>
2020-02-20 00:35:22 -0500

    nothing makes sense

--- flexpedition.ts
+++ flexpedition.ts
@@ -29,10 +29,59 @@ async function main2()
     console.log(currentlist.outputTextTable());
 }
 
+async function main3()
+{
+    var screen=makeScreen();
+
+    var mainlist=new MainExpeditionList();
+    await mainlist.loadAllExpeditions();
+
+    var currentlist=new CurrentExpeditionList2(mainlist.allExpedtionsDict);
+    mainlist.calcDifference(Object.values(currentlist.currentExpeditions)[0] as IndexExpeditionData);
+
+    var mainlistContent=mainlist.outputTextTableSorted("name").split("\n");
+
+    var mainlistHeaderElement=blessed.Text({
+        content:mainlistContent.shift()
+    });
+
+    var mainlistElement=blessed.List({
+        items:mainlistContent,
+        top:1,
+        style:{
+            selected:{
+                bg:"white",
+                fg:"black"
+            }
+        },
+        keys:true
+    });
+
+    var currentlistElement=blessed.List({
+        items:currentlist.outputTextTable().split("\n"),
+        top:32,
+        style:{
+            selected:{
+                bg:"white",
+                fg:"black"
+            }
+        },
+        keys:true
+    });
+
+    screen.append(mainlistHeaderElement);
+    screen.append(mainlistElement);
+    screen.append(currentlistElement);
+    currentlistElement.focus();
+
+    screen.render();
+}
+
 function makeScreen():Screen
 {
     var screen:Screen=blessed.screen({
-        smartCSR:true
+        smartCSR:true,
+        autoPadding:true
     });
 
     screen.title="hello";
@@ -45,4 +94,5 @@ function makeScreen():Screen
 }
 
 // main();
-main2();
+// main2();
+main3();
